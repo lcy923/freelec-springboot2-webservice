6eef4491dd423653aebdbcd48db694dc573b441c
lcy923 <lcy923@naver.com>
2023-02-01 17:07:07 +0900

    PostRepositoryTest 테스트 실행 성공(p.98)

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -1,6 +1,5 @@
 package com.jojoldu.book.springboot.web;
 
-import com.jojoldu.book.springboot.web.dto.HelloController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
