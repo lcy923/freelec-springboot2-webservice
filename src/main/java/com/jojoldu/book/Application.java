0d62041726d1c0c23f6d981d7817c944e431ff30
lcy923 <lcy923@naver.com>
2023-02-01 16:10:43 +0900

    Initial commit

---
+++ src/main/java/com/jojoldu/book/Application.java
@@ -0,0 +1,10 @@
+package com.jojoldu.book;
+
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
